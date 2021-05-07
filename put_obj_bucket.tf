4695c0f1dc978bd51fc4582e87aaa4f5a91198a2
jovonjackson03 <jovonjackson03@users.noreply.github.com>
2021-05-07 18:02:07 -0400

    test S3

--- put_obj_bucket.tf
+++ put_obj_bucket.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket_object" "object1" {
 	bucket = aws_s3_bucket.web_bucket.id
 	key    = "index.html"
-	source = "C:/Apps/terraform/tf/index.html"
+	source = "C:/Apps/terraform/tf/STACK-S3-TF/index.html"
 	acl = "public-read-write"
 	force_destroy = true
 	kms_key_id = aws_kms_key.mykey.arn
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_object" "object1" {
 resource "aws_s3_bucket_object" "object2" {
 	bucket = aws_s3_bucket.web_bucket.id
 	key    = "error.html"
-	source = "C:/Apps/terraform/tf/error.html"
+	source = "C:/Apps/terraform/tf/STACK-S3-TF/error.html"
 	acl = "public-read-write"
 	force_destroy = true
 	kms_key_id = aws_kms_key.mykey.arn
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_object" "object2" {
 resource "aws_s3_bucket_object" "object3" {
 	bucket = aws_s3_bucket.web_bucket.id
 	key    = "Stack_IT_Logo.png"
-	source = "C:/Apps/terraform/tf/Stack_IT_Logo.png"
+	source = "C:/Apps/terraform/tf/STACK-S3-TF/Stack_IT_Logo.png"
 	acl = "public-read-write"
 	force_destroy = true
 	kms_key_id = aws_kms_key.mykey.arn
