d88c76bcb1089a031762a49efdc9acfcbf9debb0
jovonjackson03 <jovonjackson03@users.noreply.github.com>
2021-05-07 16:03:25 -0400

    initial IAM

--- Stack-iam_ec2_s3.tf
+++ Stack-iam_ec2_s3.tf
@@ -1,3 +1,4 @@
+#Create User
 resource "aws_iam_user" "user" {
   name          = "stackuser1"
   path          = "/"
@@ -5,19 +6,24 @@ resource "aws_iam_user" "user" {
   permissions_boundary          = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+##Create User login credentials
 resource "aws_iam_user_login_profile" "log-prof" {
   user    = aws_iam_user.user.name
   pgp_key = "keybase:jovonjackson"
+  password_reset_required = true
 }
 
+#Output Encrypted Password
 output "password" {
   value = aws_iam_user_login_profile.log-prof.encrypted_password
 }
 
+#Create Group
 resource "aws_iam_group" "group" {
   name = "stack-group"
 }
 
+#Create Policy with S3 EC2 IAM full access
 resource "aws_iam_policy" "policy" {
   name        = "stack-iam_ec2_s3"
   description = "A policy that gives iam, ec2, and s3 access"
@@ -26,9 +32,9 @@ resource "aws_iam_policy" "policy" {
 {
   "Version": "2012-10-17",
   "Statement": [
-    { 
+    {
       "Effect": "Allow",
-      "Action": "ec2:*",
+      "Action": "iam:*",
       "Resource": "*"
     },
     {
@@ -36,9 +42,9 @@ resource "aws_iam_policy" "policy" {
       "Action": "s3:*",
       "Resource": "*"
     },
-    {
+    { 
       "Effect": "Allow",
-      "Action": "iam:*",
+      "Action": "ec2:*",
       "Resource": "*"
     }
   ]
@@ -46,25 +52,25 @@ resource "aws_iam_policy" "policy" {
 EOF
 }
 
+#Attach Policy to Group
 resource "aws_iam_policy_attachment" "stack-attach" {
   name       = "stack-attachment"
   groups     = [aws_iam_group.group.name]
   policy_arn = aws_iam_policy.policy.arn
 }
 
+#Add User to Group
 resource "aws_iam_group_membership" "team" {
   name = "stack-group-membership"
-
-  users = [
-    aws_iam_user.user.name,
-  ]
-
+  users = [aws_iam_user.user.name]
   group = aws_iam_group.group.name
 }
 
+/*
+#Enable MFA
 module "aws_mfa" {
   source = "trussworks/mfa/aws"
-
-  iam_groups = [aws_iam_group.group.name]
-  iam_users  = [aws_iam_user.user.name]
+  iam_groups = aws_iam_group.group.name
+  iam_users  = aws_iam_user.user.name
 }
+*/
